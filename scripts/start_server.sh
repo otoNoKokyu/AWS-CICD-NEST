22bcbd9bc8c4cd598fb9acc229b82e024152f3c2
otoNoKokyu <arko466@gmail.com>
2025-09-10 23:33:08 +0530

    bhbh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -9,4 +9,5 @@ cd /var/www/my-nestjs-app
 
 # Start the application with PM2
 echo "Starting the application with PM2..."
-pm2 stop all && pm2 start dist/main.js --name my-nestjs-app
+pm2 stop all && pm2 start ecosystem.config.js
+
