f48a538866cb5133d98596dc8b56a468ed4ebfd2
otoNoKokyu <arko466@gmail.com>
2025-09-10 23:03:38 +0530

    c6t7

--- ecosystem.pm2.js
+++ ecosystem.pm2.js
@@ -1,17 +1,12 @@
 module.exports = {
-  apps: [
-    {
-      name: "my-nestjs-app",
-      script: "dist/main.js",
-      instances: "max",
-      exec_mode: "cluster",
-      env: {
-        NODE_ENV: "development"
-      },
-      env_production: {
-        NODE_ENV: "production",
-        PORT: 3000
-      }
+  apps: [{
+    name: "my-nestjs-app",
+    script: "dist/main.js",
+    instances: "max",
+    exec_mode: "cluster",
+    env: {
+      NODE_ENV: "production",
+      PORT: 3000
     }
-  ]
+  }]
 };
