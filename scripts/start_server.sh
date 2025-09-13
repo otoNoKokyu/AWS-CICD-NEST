0184d61ef2a5da581bd991e6c55bb0ffb95849ac
otoNoKokyu <arko466@gmail.com>
2025-09-13 16:33:35 +0530

    c54

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
+# Source the nvm environment
 source ~/.nvm/nvm.sh || true
 nvm use default || true
-
 
 echo "Navigating to app directory..."
 cd /var/www/my-nestjs-app
 
 # Start the application with PM2
 echo "Starting the application with PM2..."
-pm2 stop all 2>/dev/null; pm2 start ecosystem.pm2.js
-
+pm2 reload ecosystem.pm2.js
