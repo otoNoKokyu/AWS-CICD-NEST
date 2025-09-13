eedbd125312379fe80a454ed6764b3b581576138
otoNoKokyu <arko466@gmail.com>
2025-09-13 19:40:17 +0530

    yoooo

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,4 +3,4 @@
 echo "Stopping the application...."
 # The "|| true" is important to prevent the deployment from failing
 # if the process is not already running (e.g., on the first deployment).
-pm2 stop my-nestjs-app || true
+pm2 stop all || true
