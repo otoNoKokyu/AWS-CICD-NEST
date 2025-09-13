edf136e638b9207c4b285e2e9f3bb9601f3ba949
otoNoKokyu <arko466@gmail.com>
2025-09-13 19:50:59 +0530

    code fixes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Sse } from '@nestjs/common';
+import { interval, map, Observable } from 'rxjs';
 
 @Controller()
 export class AppController {
@@ -25,5 +26,32 @@ export class AppController {
       tzIndia: 'Asia/Kolkata',
       tzUS: 'America/New_York'
     };
+  }
+  @Sse('api/sse-time')
+  sseTime(): Observable<MessageEvent> {
+    const indiaFormatter = new Intl.DateTimeFormat('en-US', {
+      timeZone: 'Asia/Kolkata',
+      hour: '2-digit', minute: '2-digit', second: '2-digit',
+      weekday: 'short', year: 'numeric', month: 'short', day: '2-digit'
+    });
+
+    const usFormatter = new Intl.DateTimeFormat('en-US', {
+      timeZone: 'America/New_York',
+      hour: '2-digit', minute: '2-digit', second: '2-digit',
+      weekday: 'short', year: 'numeric', month: 'short', day: '2-digit'
+    });
+
+    // Emits an event every second
+    return interval(1000).pipe(
+      map(() => {
+        const now = new Date();
+        return {
+          data: {
+            serverTimeIndia: indiaFormatter.format(now),
+            serverTimeUS: usFormatter.format(now),
+          }
+        } as MessageEvent;
+      }),
+    );
   }
 }
